946cf1e78052b6025004c5e4b98812daac6c5cf2
Anish Ghimire <anishgh94@gmail.com>
2022-07-07 15:42:54 -0400

    finished code - test cases remaining

--- src/main/java/com/survivingcodingbootcamp/blog/Populator.java
+++ src/main/java/com/survivingcodingbootcamp/blog/Populator.java
@@ -1,7 +1,9 @@
 package com.survivingcodingbootcamp.blog;
 
+import com.survivingcodingbootcamp.blog.model.Hashtag;
 import com.survivingcodingbootcamp.blog.model.Post;
 import com.survivingcodingbootcamp.blog.model.Topic;
+import com.survivingcodingbootcamp.blog.repository.HashtagRepository;
 import com.survivingcodingbootcamp.blog.repository.PostRepository;
 import com.survivingcodingbootcamp.blog.repository.TopicRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -12,40 +14,47 @@ public class Populator implements CommandLineRunner {
 
     private TopicRepository topicRepo;
     private PostRepository postRepo;
+    private HashtagRepository hashtagRepo;
 
 
-    public Populator(TopicRepository topicRepo, PostRepository postRepo) {
-
+    public Populator(TopicRepository topicRepo, PostRepository postRepo, HashtagRepository hashtagRepo) {
         this.topicRepo = topicRepo;
         this.postRepo = postRepo;
+        this.hashtagRepo = hashtagRepo;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        Hashtag hashtag = new Hashtag("#veryNice");
+        hashtagRepo.save(hashtag);
+        Hashtag hashtag2 = new Hashtag("#bad");
+        hashtagRepo.save(hashtag2);
+
         Topic topic1 = new Topic("Learning TDD");
         topicRepo.save(topic1);
+
 
         Post post1 = new Post("TDD For Fun and Profit", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.");
+                "est laborum.","Author 1",hashtag);
         postRepo.save(post1);
         Post post2 = new Post("Test the Fear Away", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.");
+                "est laborum.","Author 2",hashtag, hashtag2);
         postRepo.save(post2);
         Post post3 = new Post("Unit Tests and You", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.");
+                "est laborum.","Author 3",hashtag2);
         postRepo.save(post3);
         Topic topic2 = new Topic("Battling Imposter Syndrome");
         topicRepo.save(topic2);
--- src/main/java/com/survivingcodingbootcamp/blog/controller/HomeController.java
+++ src/main/java/com/survivingcodingbootcamp/blog/controller/HomeController.java
@@ -19,4 +19,5 @@ public class HomeController {
         model.addAttribute("topics", topicRepo.findAll());
         return "home-template";
     }
+
 }
